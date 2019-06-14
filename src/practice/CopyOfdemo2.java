0a06f6ff8ba64365b0d99551859c0f498173866d
sfoortisharma <sfoorti.sharma22@gmail.com>
2019-06-14 19:53:19 +0530

    new java class demo2

---
+++ src/practice/CopyOfdemo2.java
@@ -0,0 +1,20 @@
+package practice;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+public class CopyOfdemo2 {
+
+	public static void main(String[] args) {
+		
+		System.setProperty("webdriver.gecko.driver", "C:\\Users\\user\\Downloads\\geckodriver-v0.24.0-win64//geckodriver.exe");
+		DesiredCapabilities capabilities=DesiredCapabilities.firefox();
+	    capabilities.setCapability("marionette", true);
+		@SuppressWarnings("deprecation")
+		WebDriver driver= new FirefoxDriver(capabilities);
+		//driver.get("google.com");
+
+	}
+
+}
